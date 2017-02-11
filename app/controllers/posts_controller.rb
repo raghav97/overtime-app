acd954afd2c38e94fca82afc38dca44131f95a1a
raghav_vc <vc.raghav@gmaill.com>
2017-02-11 21:55:07 +0530

    Updated seed file and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
+  
   def index
-
+    @posts = Post.all
   end
 
   def new
@@ -11,6 +12,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    
     if @post.save!
       redirect_to @post, notice: "Post created successfully"
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(first_name: "Raghav", last_name: "Vc", email: "vc.raghav@gmail.com", password: "password", password_confirmation: "password")
+
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "A 100 posts have been created"
