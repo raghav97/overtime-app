acf8aaafd3350c5c670130e40dccfe9bbaefac3d
raghav_vc <vc.raghav@gmaill.com>
2017-02-12 15:23:06 +0530

    Itegrated growl notification for adctive record validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     
-    if @post.save!
+    if @post.save
       redirect_to @post, notice: "Post created successfully"
     else
       render :new
