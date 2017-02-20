5e0679f27a83bf9e5a5eccd04717080866ead9c7
raghav_vc <vc.raghav@gmaill.com>
2017-02-20 20:40:57 +0530

    Implemnted ability to block user when post is approved

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -28,5 +28,17 @@ describe 'navigate' do
 
       expect(page).to_not have_content('Approved')
     end
+
+    it 'should not be editable by post created when status is approves' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+      
+      expect(current_path).to eq(root_path)
+    end
   end
 end
