cb84498f11842699652956cc3659ff93432eca76
raghav_vc <vc.raghav@gmaill.com>
2017-02-22 22:41:26 +0530

    Added hours requested to the posts model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "A 100 posts have been created"
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,18 +2,21 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "Some rationale"
+    overtime_request 3.5
     user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more rationale"
+    overtime_request 0.5
     user
   end
 
   factory :post_from_another_user, class: "Post" do
     date Date.yesterday
     rationale "Some more rationale"
+    overtime_request 1.5
     non_authorized_user
   end
 end
