01e19fb0423ff6dff012b171cc0dd441ce6bbbd2
raghav_vc <vc.raghav@gmaill.com>
2017-03-26 23:16:28 +0530

    Updated the audit log factory and tested out 6 day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
-@user = User.create(first_name: "Raghav",
-                    last_name: "Vc",
-                    email: "vc.raghav@gmail.com",
-                    password: "password",
-                    password_confirmation: "password",
-                    phone: "9600129789")
+@user = User.create(first_name: "Raghav", last_name: "Vc", email: "vc.raghav@gmail.com", password: "password", password_confirmation: "password", phone: "9600129789")
 
 
 100.times do |post|
-  Post.create(date: Date.today,
-              rationale: "#{post} rationale content", 
-              user_id: @user.id, 
-              overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "A 100 posts have been created"
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "A 100 audit logs have been created"
 
 AdminUser.create(first_name: "Admin", last_name: "Vc", email: "giri@gmail.com", password: "password", password_confirmation: "password", phone: "9600129789")
 
--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :audit_log do
-    user nil
-    status 1
+    user 
+    status 0
+    start_date (Date.today - 6.days)
+    end_date nil
   end
 end
